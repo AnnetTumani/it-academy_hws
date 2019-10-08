a9d5411cef5d4fde8735ec2b615e6a3373a83b62
Annet <annet.sle.va@gmail.com>
2019-10-08 03:07:49 +0300

    On branch CountOfSymbolA

---
+++ CountOfSymbolA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CountOfSymbolA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите слово");
+            string input = Console.ReadLine();
+
+            int count = 0;
+            foreach (var letter in input.ToCharArray())
+            {
+                if (letter == 'a' || letter == 'A' || letter == 'а' || letter == 'А')
+                    count++;
+            }
+
+            Console.WriteLine("Количество букв \"а\" в слове равно: " + count);
+            Console.ReadKey();
+        }
+    }
+}
