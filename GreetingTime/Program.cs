c8ae4d6f74f0df8eb8aefd5d01e70e3948982125
Annet <annet.sle.va@gmail.com>
2019-10-08 03:00:38 +0300

    On branch GreetingTime

---
+++ GreetingTime/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (DateTime.Now)
+            {
+                case DateTime time when time.Hour > 9 && time.Hour <= 12:
+                    Console.WriteLine("Good morning, guys");
+                    break;
+                case DateTime time when time.Hour > 12 && time.Hour <= 15:
+                    Console.WriteLine("Good day, guys");
+                    break;
+                case DateTime time when time.Hour > 15 && time.Hour <= 22:
+                    Console.WriteLine("Good evening, guys");
+                    break;
+                case DateTime time when time.Hour > 22 || time.Hour <= 9:
+                    Console.WriteLine("Good night, guys");
+                    break;
+                default:
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
